998519a7b668786d31d593078ba9e45b33e805ab
Jose Viteri <Jose@Joses-MBP.home>
2018-05-14 16:29:27 -0400

    added path to home html

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(bodyParser.json());
 
 var friends = [];//empty array for friend objects
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
 app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "survey.html"));
   });
