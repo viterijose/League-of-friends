1a8286a9cdb30f22d4e3ca41dff6be5c8de4a6b2
Jose Viteri <33139338+viterijose@users.noreply.github.com>
2018-05-17 12:24:35 -0400

    updated htmlroute

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,13 +2,13 @@
 var htmlRoutes = function() {
     this.home = function () {
         app.get("/", function (request, res) {
-            res.sendFile(path.join(__dirname, "app/public/home.html"));
+            res.sendFile(path.join(__dirname, "./public/home.html"));
         })
     },
 
         this.survey = function () {
             app.get("/survey", function (request, res) {
-                res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+                res.sendFile(path.join(__dirname, "./public/survey.html"));
             })
         }
 }
