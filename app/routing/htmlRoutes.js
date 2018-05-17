2d8e3b53d430759e2226811b7d8a45190dc6d64a
Jose Viteri <Jose@Joses-MacBook-Pro.local>
2018-05-17 17:30:59 -0400

    updated server

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,10 +3,10 @@ var path = require("path");
 module.exports = function (app) {//exporting function that uses a parameter called 'app' which is an express function.
 
     app.get("/survey", function (request, res) {
-        res.sendFile(path.join(__dirname, "/../public/survey.html"));//setting routing for survey
+        res.sendFile(path.join(__dirname, "../public/survey.html"));//setting routing for survey
     })
     app.use("/", function (request, res) {
-        res.sendFile(path.join(__dirname, "/../public/home.html"));//setting routing for home
+        res.sendFile(path.join(__dirname, "../public/home.html"));//setting routing for home
     })
 }
 
