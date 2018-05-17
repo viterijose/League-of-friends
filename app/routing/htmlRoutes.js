f9df4cd4c004cdb4c639e86773d3cb7861e174cc
Jose Viteri <33139338+viterijose@users.noreply.github.com>
2018-05-17 12:39:24 -0400

    routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,14 +1,12 @@
 var path = require("path");
 
 module.exports = function (app) {
-
-    app.use("/", function (request, res) {
-        res.sendFile(path.join(__dirname, "/../public/home.html"));
-    })
-
+    
     app.get("/survey", function (request, res) {
         res.sendFile(path.join(__dirname, "/../public/survey.html"));
     })
-
+    app.use("/", function (request, res) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
+    })
 }
 
