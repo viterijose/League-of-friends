6ad1aa50f5bc6089474ba0a4079ce297e2d61fc4
Jose Viteri <33139338+viterijose@users.noreply.github.com>
2018-05-17 13:53:34 -0400

    updated post request

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,4 +5,8 @@ module.exports = function(app){//exporting function that uses a parameter called
     app.get('/api/friends', function(req,res){// /api/friends is a link found at the bottom of each page
       res.json(friends);//return an array of friends
     });
+
+    app.post('/api/friends',function(req,res){
+
+    });
 }
