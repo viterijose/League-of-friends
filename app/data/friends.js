c7e0559c79e6a16133a9200690c7d3866cbb357f
Jose Viteri <33139338+viterijose@users.noreply.github.com>
2018-05-18 22:10:31 -0400

    updated array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 
-var friendList = {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
+var friendList = [{
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
         5,
         1,
         4,
@@ -13,7 +13,7 @@ var friendList = {
         5,
         4,
         1
-      ]
-  }
+    ]
+}]
 
-  module.exports = friendList;
+module.exports = friendList;
